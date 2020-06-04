03c79339e19e580b3a0381b563b3b609e54dd0c8
Rohan Batra <rohanbatra24@gmail.com>
2020-06-04 15:23:30 -0400

    add styling

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
 	return (
-		<div className="tc bg-light-yellow dib br4 pa3 ma2 grow bw2 shadow-5">
+		<div className="tc bg-light-blue dib br4 pa3 ma2 grow bw2 shadow-5">
 			<img src={`https://robohash.org/${id}?200x200`} alt="" />
 			<div>
 				<h2>{name}</h2>
