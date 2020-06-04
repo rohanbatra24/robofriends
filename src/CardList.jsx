02e8ae6a827c220f95b4d6555c49453b70500626
Rohan Batra <rohanbatra24@gmail.com>
2020-06-04 14:34:01 -0400

    use map to create cardsArray

--- src/CardList.jsx
+++ src/CardList.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 import Card from './Card';
 
-export const CardList = ({ robots }) => {
-	return (
-		<div>
-			<Card robots={robots[0].id} name={robots[0].name} email={robots[0].email} />
-			<Card robots={robots[1].id} name={robots[1].name} email={robots[1].email} />
-			<Card robots={robots[2].id} name={robots[2].name} email={robots[2].email} />
-		</div>
-	);
+const CardList = ({ robots }) => {
+	const cardsArray = robots.map((robot) => {
+		return <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email} />;
+	});
+
+	return <div>{cardsArray}</div>;
 };
 
 export default CardList;
