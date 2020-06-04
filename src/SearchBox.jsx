72b233bd4110a397379cd8933ffabea7086b85f1
Rohan Batra <rohanbatra24@gmail.com>
2020-06-04 15:05:23 -0400

    add on change handler

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-export const SearchBox = () => {
+export const SearchBox = ({ searchChange }) => {
 	return (
 		<div className="pa2">
-			<input className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="search robots" />
+			<input
+				onChange={searchChange}
+				className="pa3 ba b--green bg-lightest-blue"
+				type="search"
+				placeholder="search robots"
+			/>
 		</div>
 	);
 };
