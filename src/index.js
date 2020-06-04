16e936216a2cbf1e6c3c1225a9b3b2afcf562df8
Rohan Batra <rohanbatra24@gmail.com>
2020-06-04 14:34:15 -0400

    render app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { robots } from './robots';
-import CardList from './CardList';
+import App from './App';
 
 import 'tachyons';
 
 import './index.css';
-// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<CardList robots={robots} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
