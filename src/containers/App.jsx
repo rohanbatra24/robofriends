e132b9451244984cc8ce6810cb668146922aeff3
Rohan Batra <rohanbatra24@gmail.com>
2020-06-05 12:59:00 -0400

    wrap ecard list in error boundary

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './app.css';
 
 class App extends Component {
@@ -39,7 +40,9 @@ class App extends Component {
 						<h1 className="f1">RoboFriends</h1>
 						<SearchBox searchChange={this.onSearchChange} />
 						<Scroll>
-							<CardList robots={filteredRobots} />
+							<ErrorBoundary>
+								<CardList robots={filteredRobots} />
+							</ErrorBoundary>
 						</Scroll>
 					</div>
 				</div>
