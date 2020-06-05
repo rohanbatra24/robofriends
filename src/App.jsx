075ab0a8c5e80077e24b4764a9f0b4f52d061d0a
Rohan Batra <rohanbatra24@gmail.com>
2020-06-05 11:29:55 -0400

    add componentDidMount to make fetch requests to json placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-
 import CardList from './CardList';
-
-import { robots } from './robots';
-import { SearchBox } from './SearchBox';
-
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './app.css';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			robots      : robots,
+			robots      : [],
 			searchField : ''
 		};
+		console.log('constuctor');
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((users) => this.setState({ robots: users }));
 	}
 
 	onSearchChange = (event) => {
@@ -24,15 +28,22 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter((robot) =>
 			robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
 		);
-		return (
-			<div>
-				<div className="tc">
-					<h1 className="f1">RoboFriends</h1>
-					<SearchBox searchChange={this.onSearchChange} />
-					<CardList robots={filteredRobots} />
+
+		if (this.state.robots.length === 0) {
+			return <h1>Loading</h1>;
+		}
+		else
+			return (
+				<div>
+					<div className="tc">
+						<h1 className="f1">RoboFriends</h1>
+						<SearchBox searchChange={this.onSearchChange} />
+						<Scroll>
+							<CardList robots={filteredRobots} />
+						</Scroll>
+					</div>
 				</div>
-			</div>
-		);
+			);
 	}
 }
 
